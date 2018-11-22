7a47c6bdb64bbd32bc085454bd22df31eefe567c
Rob J Hyndman <robjhyndman@gmail.com>
2018-11-23 10:37:38 +1100

    Name of template set to hyndman

--- R/formats.R
+++ R/formats.R
@@ -3,7 +3,6 @@
 #' Each function is a wrapper for \code{\link[bookdown]{pdf_document2}} to
 #' produce curriculum vitae
 #'
-#' @param template Name of template to be used.
 #' @param \dots Arguments passed to \code{\link[bookdown]{pdf_document2}}.
 #'
 #' @return An R Markdown output format object.
@@ -11,12 +10,8 @@
 #' @author Rob J Hyndman
 #'
 #' @export
-cv <- function(template="hyndman",...) {
-  template <- system.file("rmarkdown", "templates", template, "resources",
-    paste0(template, "template.tex"), package="vitae")
-   bookdown::pdf_document2(...,
-     template = template
-   )
+hyndman <- function(...) {
+  template <- system.file("rmarkdown", "templates", "hyndman",
+  	"resources", "hyndmantemplate.tex", package="vitae")
+   bookdown::pdf_document2(..., template = template)
 }
-
-
