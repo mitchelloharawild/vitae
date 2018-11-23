d7319cfcc7d159c9b6a111bd1553ced72f942f4a
Mitchell <mitch.ohara-wild@monash.edu>
2018-11-23 14:15:29 +1100

    Added brief_entries

---
+++ R/brief.R
@@ -0,0 +1,25 @@
+#' @importFrom rlang enexpr expr_text !!
+#' @export
+brief_entries <- function(data, what, when, with){
+  edu_exprs <- list(
+    what = enexpr(what) %missing% NA,
+    when = enexpr(when) %missing% NA,
+    with = enexpr(with) %missing% NA
+  )
+
+  data <- dplyr::transmute(data,  !!!edu_exprs)
+
+  add_class(data, "vitae_brief")
+}
+
+#' @importFrom knitr knit_print
+#' @export
+knit_print.vitae_brief <- function(x, options){
+  x[is.na(x)] <- ""
+
+  out <- glue_data(x, "\\briefitem{<<what>>}{<<when>>}{<<with>>}",
+                   .open = "<<", .close = ">>")
+
+  knitr::asis_output(glue("\\briefsection{<<glue_collapse(out)>>}",
+                          .open = "<<", .close = ">>"))
+}
