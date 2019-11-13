b1dd8de9358b95265143a662a92400eab7561cf4
Yihui Xie <xie@yihui.name>
2019-11-12 19:47:48 -0600

    Update test-entries.R

--- tests/testthat/test-entries.R
+++ tests/testthat/test-entries.R
@@ -38,7 +38,7 @@ test_that("detailed_entries", {
   print <- knitr::knit_print(entries)
 
   expect_match(print, "detailedsection")
-  expect_equal(NROW(entries), 2)
+  expect_equal(NROW(entries), length(RefManageR::ReadBib(tmpbib)))
   expect_equal(stringr::str_count(print, "detaileditem"), 2)
 
   expect_equal(stringr::str_count(print, "\\\\item"), 2)
