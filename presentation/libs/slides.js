eb98462fe5a01d326ed6ca68545d0d0d559479de
Mitch <mitch.ohara-wild@monash.edu>
2020-10-08 16:55:06 +1100

    Remove non-package directories

--- presentation/libs/slides.js
+++
@@ -1,8 +0,0 @@
- $(document).ready(function(){
-  slideshow.on('beforeShowSlide', function (slide) {
-    $("video").each(function() {
-      $(this).get(0).currentTime = 0;
-      $(this).get(0).play();
-    });
-  });
- });
