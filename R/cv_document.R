44dad762c3a7c342c43304675408c5ab4b04e2b7
Mitch <mitch.ohara-wild@monash.edu>
2020-10-08 13:05:14 +1100

    Drop unused cv_document helpers

--- R/cv_document.R
+++ R/cv_document.R
@@ -48,59 +48,6 @@ cv_document <- function(..., pandoc_args = NULL, pandoc_vars = NULL) {
   out
 }
 
-flatten_meta <- function(knit_meta, test) {
-  all_meta <- list()
-  for (dep in knit_meta) {
-    if (is.null(names(dep)) && is.list(dep)) {
-      inner_meta <- flatten_meta(dep, test)
-      all_meta <- append(all_meta, inner_meta)
-    } else if (test(dep)) {
-      all_meta[[length(all_meta) + 1]] <- dep
-    }
-  }
-  all_meta
-}
-
-latex_dependencies_as_string <- function(dependencies) {
-  lines <- sapply(dependencies, function(dep) {
-    opts <- paste(dep$options, collapse = ",")
-    if (opts != "") opts <- paste0("[", opts, "]")
-    # \\usepackage[opt1,opt2]{pkgname}
-    paste0("\\usepackage", opts, "{", dep$name, "}")
-  })
-  paste(unique(lines), collapse = "\n")
-}
-
-bibliography_header <- function(bibs) {
-  titles <- lapply(bibs, function(x) x[["title"]])
-  files <- lapply(bibs, function(x) x[["file"]])
-  c(
-    paste0("\\DeclareBibliographyCategory{", titles, "}"),
-    paste0("\\bibliography{", paste0(unique(files), collapse = ", "), "}")
-  )
-}
-
-has_meta <- function(knit_meta, class) {
-  if (inherits(knit_meta, class)) {
-    return(TRUE)
-  }
-
-  if (is.list(knit_meta)) {
-    for (meta in knit_meta) {
-      if (is.null(names(meta))) {
-        if (has_meta(meta, class)) {
-          return(TRUE)
-        }
-      } else {
-        if (inherits(meta, class)) {
-          return(TRUE)
-        }
-      }
-    }
-  }
-  FALSE
-}
-
 copy_supporting_files <- function(template) {
   path <- system.file("rmarkdown", "templates", template, "skeleton", package = "vitae")
   files <- list.files(path)
