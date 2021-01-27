b7a09aeffd2ddaa6955200a1b88b17174b68304d
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-01-27 12:32:12 +1100

    Fix incomplete cv_document param tag

--- R/cv_document.R
+++ R/cv_document.R
@@ -7,7 +7,6 @@
 #' @inheritParams bookdown::pdf_document2
 #' @param ... Arguments passed to rmarkdown::pdf_document().
 #' @param pandoc_vars Pandoc variables to be passed to the template.
-#' @para
 #'
 #' @export
 cv_document <- function(..., pandoc_args = NULL, pandoc_vars = NULL,
