63fb243cbf00b37327bbf2dee7acea65fe934fa8
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2025-12-01 04:45:40 -0500

    Fix casing in comment

--- R/twentyseconds.R
+++ R/twentyseconds.R
@@ -2,7 +2,7 @@
 #'
 #' A curriculum vitae, otherwise known as a CV or résumé, is a document used by
 #' individuals to communicate their work history, education and skill set. This
-#' is a style template for your curriculum written in LaTex. The main goal of
+#' is a style template for your curriculum written in LaTeX. The main goal of
 #' this template is to provide a curriculum that is able to survive to the
 #' résumés screening of "twenty seconds":
 #' https://github.com/spagnuolocarmine/TwentySecondsCurriculumVitae-LaTex
